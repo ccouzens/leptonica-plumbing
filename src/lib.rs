30d07f9fc5b99e599b0855c69eb916a54fcae7eb
Kevin Xiao <xlzqwerty1@gmail.com>
2022-08-15 17:28:32 -0700

    Update impl with AsRef

--- src/lib.rs
+++ src/lib.rs
@@ -17,51 +17,48 @@ pub use pix::{Pix, PixReadError, PixReadMemError};
 pub use pixa::Pixa;
 pub use r#box::{Box, BoxCreateValidError};
 
-/// Wrapper container for leptonica's version and image lib versions strings.
+/// Wrapper for leptonica strings
 #[derive(Debug)]
-pub struct LeptonicaVersion {
-    version: *mut c_char,
-    image_lib_versions: *mut c_char,
+pub struct LeptonicaString {
+    value: *mut c_char,
 }
 
-impl Drop for LeptonicaVersion {
+impl AsRef<str> for LeptonicaString {
+    fn as_ref(&self) -> &str {
+        unsafe {
+            CStr::from_ptr(self.value)
+                .to_str()
+                .expect("failed to get leptonica string")
+        }
+    }
+}
+
+impl AsRef<CStr> for LeptonicaString {
+    fn as_ref(&self) -> &CStr {
+        unsafe { CStr::from_ptr(self.value) }
+    }
+}
+
+impl Drop for LeptonicaString {
     fn drop(&mut self) {
-        unsafe {
-            free(self.version as *mut c_void);
-            free(self.image_lib_versions as *mut c_void);
-        }
+        unsafe { free(self.value as *mut c_void) }
     }
 }
 
-/// Returns a struct containing leptonica's version and image lib versions strings,
-/// and will automatically free the memory for these C-strings when out of scope.
-impl LeptonicaVersion {
-    pub fn new() -> Self {
-        LeptonicaVersion {
-            version: unsafe { getLeptonicaVersion() },
-            image_lib_versions: unsafe { getImagelibVersions() },
-        }
+/// Wrapper for [`getLeptonicaVersion`](https://github.com/DanBloomberg/leptonica/blob/1.82.0/src/utils1.c#L970-L982)
+///
+/// Returns the version identifier as a LeptonicaString.
+pub fn get_version() -> LeptonicaString {
+    LeptonicaString {
+        value: unsafe { getLeptonicaVersion() },
     }
+}
 
-    /// Wrapper for [`getLeptonicaVersion`](https://github.com/DanBloomberg/leptonica/blob/1.82.0/src/utils1.c#L970-L982)
-    ///
-    /// Returns the version identifier as a static string.
-    pub fn get_version(&self) -> &'static str {
-        unsafe {
-            CStr::from_ptr(self.version)
-                .to_str()
-                .expect("failed to call getLeptonicaVersion")
-        }
-    }
-
-    /// Wrapper for [`getImagelibVersions`](https://github.com/DanBloomberg/leptonica/blob/1.82.0/src/libversions.c#L82-L102)
-    ///
-    /// Returns the image lib version identifiers as a static string.
-    pub fn get_imagelib_versions(&self) -> &'static str {
-        unsafe {
-            CStr::from_ptr(self.image_lib_versions)
-                .to_str()
-                .expect("failed to call getImagelibVersions")
-        }
+/// Wrapper for [`getImagelibVersions`](https://github.com/DanBloomberg/leptonica/blob/1.82.0/src/libversions.c#L82-L102)
+///
+/// Returns the image lib version identifiers as a LeptonicaString.
+pub fn get_imagelib_versions() -> LeptonicaString {
+    LeptonicaString {
+        value: unsafe { getImagelibVersions() },
     }
 }
